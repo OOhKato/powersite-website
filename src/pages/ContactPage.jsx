d4b34c97bca5d4cbec30d172b9ed01cc29c48e36
DarekDLD <darekdld@gmail.com>
2026-06-22 17:04:10 +0200

    Add files via upload

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,9 +2,9 @@ import SubpageLayout from '../components/SubpageLayout'
 import Contact from '../components/Contact'
 
 export default function ContactPage() {
-    return (
-        <SubpageLayout>
-            <Contact standalone />
-        </SubpageLayout>
-    )
+  return (
+    <SubpageLayout>
+      <Contact standalone />
+    </SubpageLayout>
+  )
 }
